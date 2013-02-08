d93678fc467ffeb6470f6fce371981f6364dc1d2
Pascal Guay <pascalg@gmail.com>
2013-02-08 17:03:22 -0500

    Simple A.I. with Distance Verification
    The clones now stops when reaching the main character.
    Started the implementation of the master to control each instances of
    the clones.

--- Assets/Mecanim Assets/Mecanim/Scripts/CharacterAI.cs
+++ Assets/Mecanim Assets/Mecanim/Scripts/CharacterAI.cs
@@ -4,38 +4,53 @@ using System.Collections.Generic;
 
 public class CharacterAI : MonoBehaviour {
 	
-    Transform target; //the player's target
+	private TimeActions timeActions;
+	
+	/* TODO: Create a Character class with the subclasses being the main characters and the clones
+	 * to facilitate all their implementations.
+	 */ 
+    /*Transform playerTransform //the player's position
+	{
+		get{
+			return master.transform;
+		}
+		set{
+			playerTransform=value;
+		}
+	}*/
+	Transform playerTransform;
+	Transform cloneTransform; //current transform data of this clone
     private float moveSpeed = 3.0f; //move speed
     private float rotationSpeed = 3.0f; //speed of turning
-	
-    Transform myTransform; //current transform data of this enemy
-	
-	private TimeActions timeActions;
+	private static CharControl master;
 	
     void Awake()
     {
-    	myTransform = transform; //cache transform data for easy access/preformance
+    	cloneTransform = transform; //cache transform data for easy access/preformance
     }
      
     void Start()
     {
-	    target = GameObject.FindWithTag("Player").transform; //target the player
 		timeActions = GetComponent<TimeActions>();
+		master = GetComponent<CharControl>();
+		playerTransform = GameObject.FindWithTag("Player").transform;
     }
      
-    void Update () {	
+    void Update () {
 		for(int i = 0; i < timeActions.GetCloneCount(); ++i)
 		{
 			GameObject currentClone = GameObject.Find("clone"+i);
 			if (currentClone)
 			{
-				myTransform = currentClone.transform; //target the player
-			    //rotate to look at the player
-			    myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-			    Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
-			     
-			    //move towards the player
-			    myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+				cloneTransform = currentClone.transform; //target the player
+				if(Vector3.Distance(cloneTransform.position, playerTransform.position) > 1.0f)
+				{
+				    //rotate to look at the player
+				    cloneTransform.rotation = Quaternion.Slerp(cloneTransform.rotation,
+				    Quaternion.LookRotation(playerTransform.position - cloneTransform.position), rotationSpeed*Time.deltaTime);
+				    //move towards the player
+				    cloneTransform.position += cloneTransform.forward * moveSpeed * Time.deltaTime;
+				}
 			}
 		}
     }
