3a470acb3cf1186601cec46b0091b8cfaf053578
Mads Rønnow <roennowmads@gmail.com>
2013-03-01 04:48:28 -0500

    Fixed neckturn

--- Assets/Mecanim Assets/Mecanim/Scripts/Headturn.cs
+++ Assets/Mecanim Assets/Mecanim/Scripts/Headturn.cs
@@ -3,24 +3,39 @@ using System.Collections;
 
 public class Headturn : MonoBehaviour {
 	
+	private float prevAngle = 0.0f;
+	private Transform camTransform;
+	private Transform playerTransform;
+	
+	private Transform headTransform;
 	
 	// Use this for initialization
 	void Start () {
+		camTransform = Camera.main.transform;
+		playerTransform = transform;
+		
+		headTransform = GameObject.Find("Bip001 Head").transform;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		float camRot = Camera.main.transform.eulerAngles.y;
-		float playerRot = transform.root.eulerAngles.y;
+		float camRot = camTransform.eulerAngles.y;
+		float playerRot = playerTransform.eulerAngles.y;
 		
-		Debug.Log(camRot - playerRot);
-		
-		float difference = Mathf.Min(Mathf.Max(camRot - playerRot, -65.0f), 80.0f) - 270.0f;
-		
-		Quaternion rotation = Quaternion.Euler(0.0f, difference, -90.0f);
-	    transform.rotation = rotation;
-		
-	//	Debug.Log(rotation);
+		float difference = camRot - playerRot;
+			
+		if (difference > -80.0f && difference < 80.0f) { 
+			prevAngle = Mathf.LerpAngle(prevAngle, camRot - 90.0f, .2f);
+			Quaternion rotation = Quaternion.Euler(0.0f, prevAngle, -90.0f);
+	    	headTransform.rotation = rotation;
+		}
+		//if the camera is looking behind the player, the head should turn to face the front.
+		else {
+			Quaternion rotation = Quaternion.Euler(0.0f, prevAngle, -90.0f);
+	    	headTransform.rotation = rotation;
+			
+			prevAngle = Mathf.LerpAngle(prevAngle, playerRot - 90.0f, .05f);
+		}
 	}
 }
 			
