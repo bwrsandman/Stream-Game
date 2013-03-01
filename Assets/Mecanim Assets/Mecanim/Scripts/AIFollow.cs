d7af11685fdae9c54784411c499f386f64b24a82
Pascal Guay <pascalg@gmail.com>
2013-03-01 15:44:19 -0500

    NavMesh Update on Level and Clones Animation
    Added navmesh in the pre-prototype level and on the level001. Added
    radius around the main player to make the clone stop when near him.
    Added animation, the character now animates.

--- Assets/Mecanim Assets/Mecanim/Scripts/AIFollow.cs
+++ Assets/Mecanim Assets/Mecanim/Scripts/AIFollow.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 using System.Collections;
 
 public class AIFollow : MonoBehaviour {
+	private Animator anim;							// a reference to the animator on the character
 	public Transform m_Player;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	GetComponent<NavMeshAgent>().destination = m_Player.position;
+void Update () {
+	float charSpeed;
+
+	if(Vector3.Distance(transform.position, m_Player.position) > 1.5f)
+	{
+		GetComponent<NavMeshAgent>().enabled = true;
+		GetComponent<NavMeshAgent>().destination = m_Player.position;
+		charSpeed = GetComponent<NavMeshAgent>().speed;
 	}
+	else
+	{
+		GetComponent<NavMeshAgent>().enabled = false;
+		charSpeed = 0.0f;
+	}
+	anim.SetFloat("Speed", charSpeed);
+}
+	
 }
