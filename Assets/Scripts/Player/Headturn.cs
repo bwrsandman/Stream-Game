ed72ae39b6cbeebb89010395c10270a200f1b861
Mads Rønnow <roennowmads@gmail.com>
2013-03-03 06:21:55 -0500

    Fixed head turning bug
    Fixed a head turning bug, which was caused by the head angle lerp after
    the player changed orientation.
    E.g.: If the player turns 180 degrees, the head will slowly lerp from
    where it was facing to the new orientation.
    This has been fixed by checking for the delta angle, and if it exceeds
    80 degrees, the lerp is replaced by a simple assignment to the
    destination rotation.

--- Assets/Scripts/Player/Headturn.cs
+++ Assets/Scripts/Player/Headturn.cs
@@ -20,19 +20,30 @@ public class Headturn : MonoBehaviour {
 		float camRot = camTransform.eulerAngles.y;
 		float playerRot = playerTransform.eulerAngles.y;
 		
-		float difference = camRot - playerRot;
+		float playerCamDiff = Mathf.DeltaAngle(camRot,playerRot);
+		float deltaAngle = Mathf.Abs(Mathf.DeltaAngle(prevAngle, playerRot - 90.0f));
+		
+		if (playerCamDiff > -80.0f && playerCamDiff < 80.0f) { 
 			
-		if (difference > -80.0f && difference < 80.0f) { 
-			prevAngle = Mathf.LerpAngle(prevAngle, camRot - 90.0f, .2f);
-			Quaternion rotation = Quaternion.Euler(0.0f, prevAngle, -90.0f);
-	    	headTransform.rotation = rotation;
+			//don't lerp if the change in angle is greater than 80 degrees:
+			if (Mathf.Ceil(deltaAngle) < 80.0f) {
+				prevAngle = Mathf.LerpAngle(prevAngle, camRot - 90.0f, .2f);
+				Quaternion rotation = Quaternion.Euler(0.0f, prevAngle, -90.0f);
+		    	headTransform.rotation = rotation;
+			}
+			else
+				prevAngle = playerRot - 90.0f;
 		}
 		//if the camera is looking behind the player, the head should turn to face the front.
 		else {
 			Quaternion rotation = Quaternion.Euler(0.0f, prevAngle, -90.0f);
 	    	headTransform.rotation = rotation;
 			
-			prevAngle = Mathf.LerpAngle(prevAngle, playerRot - 90.0f, .05f);
+			//if the player turns more than 80 degrees on the spot, don't lerp:
+			if (deltaAngle < 80.0f && deltaAngle > 0.1) 
+				prevAngle = Mathf.LerpAngle(prevAngle, playerRot - 90.0f, .05f);
+			else
+				prevAngle = playerRot - 90.0f;
 		}
 	}
 }
