f1ded826fdd1f31b717a38281bcea83ba8717ee3
Sandy Carter <mr.sandy.carter@gmail.com>
2013-03-19 22:43:40 -0400

    Activation indicator now pops up when you can activate activatable objects

--- Assets/Scripts/Player/ActivationHandler.cs
+++ Assets/Scripts/Player/ActivationHandler.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class ActivationHandler : MonoBehaviour {
 	
 	public Activateable selectedObject;
+	public GameObject activationPrefab;
+	public GameObject activationTexture;
+	
 	WeaponHandler weaponHandler;
 	PackHandler packHandler;
 	
@@ -15,14 +18,25 @@ public class ActivationHandler : MonoBehaviour {
 	
 	public void setSelectedObject (Activateable obj)
 	{
+		// Can't select currently held weapon
 		if(obj != weaponHandler.weapon)
+		{
 			selectedObject = obj;
+			Destroy(activationTexture);
+			Vector3 distance = gameObject.transform.position - obj.transform.position;
+			distance.y = 0.0f;
+			Quaternion rotation = Quaternion.Euler(90.0f, Quaternion.LookRotation(distance).eulerAngles.y, 0.0f);
+			activationTexture = (GameObject)Instantiate(activationPrefab, obj.rigidbody.transform.position + Vector3.up, rotation);
+		}
 	}
 	
 	public void unsetSelectedObject (Activateable obj)
 	{
 		if(selectedObject == obj)
+		{
 			selectedObject = null;
+			Destroy(activationTexture);
+		}
 	}
 	
 	void Activate ()
@@ -51,5 +65,13 @@ public class ActivationHandler : MonoBehaviour {
 			Activate();
 		}
 		
+		// Update texture rotation
+		if (activationTexture)
+		{
+			Vector3 distance = gameObject.transform.position - activationTexture.transform.position;
+			distance.y = 0.0f;
+			Quaternion rotation = Quaternion.Euler(90.0f, Quaternion.LookRotation(distance).eulerAngles.y, 0.0f);
+			activationTexture.transform.rotation = rotation;
+		}
 	}
 }
